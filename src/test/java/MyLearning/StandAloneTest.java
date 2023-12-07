d990341f1f5ff30d899965af81407883d547d798
Amit <amitkumar225176@gmail.com>
2023-12-07 21:22:18 +0530

    StandAlone file is added

---
+++ src/test/java/MyLearning/StandAloneTest.java
@@ -0,0 +1,51 @@
+package MyLearning;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		
+		String productName = "ZARA COAT 3";
+		driver.get("https://rahulshettyacademy.com/client/");
+		driver.findElement(By.id("userEmail")).sendKeys("amitkr@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Passwork@007");
+		driver.findElement(By.id("login")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));	
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+		WebElement prod = products.stream().filter(product->product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+		
+//		wait till msg displayed Product added to cart 		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		
+//		wait till page load
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();		
+		List<WebElement> cartProducts = driver.findElements(By.xpath("//div[@class=\"cartSection\"]/h3"));
+		boolean match = cartProducts.stream().anyMatch(cartp->cartp.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);	
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+		
+
+	}
+ 
+}
